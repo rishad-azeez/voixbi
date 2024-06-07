7a80054f80776e0e1cc368a5bd8d7ba3380ac2c5
William Wernert <william.wernert@grafana.com>
2024-06-07 12:59:18 -0400

    CHANGE ME: impl prom write to ds uid

--- pkg/services/ngalert/writer/writer.go
+++ pkg/services/ngalert/writer/writer.go
@@ -7,6 +7,10 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 )
 
+type WriterFactory interface {
+	GetWriter(ctx context.Context, orgID int64) (Writer, error)
+}
+
 type Writer interface {
 	Write(ctx context.Context, name string, t time.Time, frames data.Frames, extraLabels map[string]string) error
 }
