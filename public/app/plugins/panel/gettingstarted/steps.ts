90fcf55757de7e7f4ec70ee410cc4dcfae126e70
francischacko19 <francis@voix.me>
2024-06-30 16:34:43 +0300

    made changes in tutorial section

--- public/app/plugins/panel/gettingstarted/steps.ts
+++ public/app/plugins/panel/gettingstarted/steps.ts
@@ -4,7 +4,7 @@ import { getDatasourceSrv } from 'app/features/plugins/datasource_srv';
 
 import { SetupStep } from './types';
 
-const step1TutorialTitle = 'Grafana fundamentals';
+const step1TutorialTitle = ' VOIXBI fundamentals';
 const step2TutorialTitle = 'Create users and teams';
 const keyPrefix = 'getting.started.';
 const step1Key = `${keyPrefix}${step1TutorialTitle.replace(' ', '-').trim().toLowerCase()}`;
@@ -22,8 +22,8 @@ export const getSteps = (): SetupStep[] => [
         type: 'tutorial',
         heading: 'Data source and dashboards',
         title: step1TutorialTitle,
-        info: 'Set up and understand Grafana if you have no prior experience. This tutorial guides you through the entire process and covers the “Data source” and “Dashboards” steps to the right.',
-        href: 'https://grafana.com/tutorials/grafana-fundamentals',
+        info: 'Set up and understand VOIXBI if you have no prior experience. This tutorial guides you through the entire process and covers the “Data source” and “Dashboards” steps to the right.',
+        href: 'fundamentals',
         icon: 'grafana',
         check: () => Promise.resolve(store.get(step1Key)),
         key: step1Key,
@@ -34,7 +34,7 @@ export const getSteps = (): SetupStep[] => [
         title: 'Add your first data source',
         heading: 'data sources',
         icon: 'database',
-        learnHref: 'https://grafana.com/docs/grafana/latest/features/datasources/add-a-data-source',
+        learnHref: 'https://.com/docs//latest/guides/getting_started/#create-a-dashboard',
         href: 'datasources/new',
         check: () => {
           return new Promise((resolve) => {
@@ -55,7 +55,7 @@ export const getSteps = (): SetupStep[] => [
         title: 'Create your first dashboard',
         icon: 'apps',
         href: 'dashboard/new',
-        learnHref: 'https://grafana.com/docs/grafana/latest/guides/getting_started/#create-a-dashboard',
+        learnHref: 'https://.com/docs//latest/guides/getting_started/#create-a-dashboard',
         check: async () => {
           const result = await getBackendSrv().search({ limit: 1 });
           return result.length > 0;
@@ -77,7 +77,7 @@ export const getSteps = (): SetupStep[] => [
         heading: 'Users',
         title: 'Create users and teams',
         info: 'Learn to organize your users in teams and manage resource access and roles.',
-        href: 'https://grafana.com/tutorials/create-users-and-teams',
+        href: 'https://.com/docs//latest/guides/getting_started/#create-a-dashboard',
         icon: 'users-alt',
         key: step2Key,
         check: () => Promise.resolve(store.get(step2Key)),
@@ -87,7 +87,7 @@ export const getSteps = (): SetupStep[] => [
         type: 'docs',
         heading: 'plugins',
         title: 'Find and install plugins',
-        learnHref: 'https://grafana.com/docs/grafana/latest/plugins/installation',
+        learnHref: 'https://.com/docs//latest/guides/getting_started/#create-a-dashboard',
         href: 'plugins',
         icon: 'plug',
         check: async () => {
